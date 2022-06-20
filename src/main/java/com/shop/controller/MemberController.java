2ae1e4775541f44787effee355901450a03aef1b
조중현 <junghyen2003@gmail.com>
2022-06-20 16:28:03 +0900

    Add sign up validation

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -26,11 +29,24 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto) {
-      Member member = Member.createMember(memberFormDto, passwordEncoder);
-      memberService.saveMember(member);
+    // 검증하려는 객체에 @Valid 어노테이션을 선언하고, 파라미터로 bindingResult 객체를 추가
+    // 검사 후 결과는 bindingResult 에 담김
+    public String memberForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        // 에러 판단
+        if (bindingResult.hasErrors()) {
+           return "member/memberForm";
+        }
 
-      return "redirect:/";
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            // 중복 에러 발생 시 에러 메시지를 뷰로 전달
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
     }
 
 
