496729c38484851e16c4732a6f2abe9e2bce4ef3
조중현 <junghyen2003@gmail.com>
2022-06-21 14:28:53 +0900

    Add entity mapping

---
+++ src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,28 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    // 회원 엔티티와 일대일(단방향)로 매핑
+    // 회원 엔티티에는 장바구니에 연관된 소스가 없음
+    @JoinColumn(name = "member_id")
+    // @JoinColumn 어노테이션을 이용해 매핑할 외래키를 지정. name 속성에는 매핑할 외래키의 이름을 설정
+    // @JoinColumn의 name을 명시하지 않으면 JPA가 알아서 ID를 찾지만 컬럼명이 원하는대로 생성되지 않는 경우가 있기 때문에 직접 지정
+    private Member member;
+}
---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,52 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+// 정렬 시 사용하는 키워드 "order"을 회피하기 위한 "orders"로 테이블 이름 변경
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    // 한 명의 회원이 여러번 주문을 할 수 있으므로 주문 엔티티 기준에서 다대일(n:1) 단방향 매핑
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    /**
+     * 주문일
+     */
+    private LocalDateTime orderDate;
+
+    /**
+     * 주문 상태
+     */
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order")
+    // 주문 상품 엔티티와 일대다 매핑
+    // 외래키(order_id)가 order_item 테이블에 있으므로 연관관계의 주인은 OrderItem 엔티티
+    // Order 엔티티가 주인이 아니므로 "mappedBy" 속성으로 연관 관계의 주인을 설정
+    // mappedBy = "order" 속성 값의 의미는 OrderItem에 있는 Order에 의해 관리된다는 의미
+    // 즉 연관 관계의 주인 필드인 order를 mappedBy의 값으로 세팅
+    private List<OrderItem> orderItems = new ArrayList<>();
+    // 하나의 주문이 여러 개의 주문 상품을 갖으므로 List 자료형을 사용해서 매핑
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
