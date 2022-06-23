b78f2f36d92716b2273397d4626d82640df7d064
조중현 <junghyen2003@gmail.com>
2022-06-23 15:15:07 +0900

    Add update cart item

--- src/main/java/com/shop/repository/CartItemRepository.java
+++ src/main/java/com/shop/repository/CartItemRepository.java
@@ -1,9 +1,25 @@
 package com.shop.repository;
 
+import com.shop.dto.CartDetailDto;
 import com.shop.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    // CartDetailDto의 생성자를 이용하여 DTO를 반환
+    // 생성자의 파라미터 순서는 DTO 클래스에 명시한 순서로 넣어주어야 함
+    @Query("select new com.shop.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+            "from CartItem ci, ItemImg im " +
+            "join ci.item i " +
+            "where ci.cart.id = :cartId " +
+            "and im.item.id = ci.item.id " +
+            "and im.repimgYn = 'Y' " +
+            "order by ci.regTime desc"
+    )
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
 }
